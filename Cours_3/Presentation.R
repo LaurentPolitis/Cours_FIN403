27707053f0540401444101d70963d6cca9b0cbb3
LaurentPolitis <politis.laurent@gmail.com>
2018-04-06 11:55:40 +0200

    nex presentation

---
+++ Cours_3/Presentation.R
@@ -0,0 +1,13 @@
+---
+  title: "Data visualisation application"
+author: "Laurent Politis"
+date: "6 avril 2018"
+output:   
+  html_document:
+  fig_caption: yes
+highlight: pygments
+keep_md: yes
+theme: journal
+toc: yes
+---
+  
