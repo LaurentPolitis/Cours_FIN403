57cf0e6d31ead1a6df863842ea80713a4667afac
LaurentPolitis <politis.laurent@gmail.com>
2021-04-26 11:54:11 +0200

    add new version of datamining communes

--- Cours_1/cours_data_visualisation/data_mining_communes.R
+++ Cours_1/cours_data_visualisation/data_mining_communes.R
@@ -17,8 +17,8 @@ library(readr)
 # population_ville <- read.csv(paste(PATH,"population_ville.csv",sep=""), sep=";", stringsAsFactors=FALSE,row.names = "CODGEO" ) 
 
 population_ville <- read_delim("https://raw.githubusercontent.com/LaurentPolitis/Data_Viz_Fin403/master/data/population_ville.csv",
-";", escape_double = FALSE, locale = locale(encoding = "ISO-8859-1"),
-trim_ws = TRUE)
+                               ";", escape_double = FALSE, locale = locale(encoding = "ISO-8859-1"),
+                               trim_ws = TRUE)
 
 # sous forme de data frame 
 #____________________________________________________________________________________________
@@ -27,48 +27,51 @@ class(population_ville)
 
 # explorer les données sous R studio 
 #____________________________________________________________________________________________
-  View(population_ville)
- 
+View(population_ville)
+
 # voir le début et la fin d'un fichier 
 #____________________________________________________________________________________________
 
-  head(population_ville)
-  tail(population_ville)
-   
+head(population_ville)
+tail(population_ville)
+
 #  la dimension de la data frame 
 #___________________________________________________________________________________________________
-  
-  dim_pop = dim(population_ville) # la fonction dim renvoie le nombre de ligne puis le nombre de colonne 
-  
+
+dim_pop = dim(population_ville) # la fonction dim renvoie le nombre de ligne puis le nombre de colonne 
+
 # complete.cases() renvoie un vecteur de boolean TRUE si la ligne est compléte FALSE sinon 
 #____________________________________________________________________________________________  
-  
- vec_na = complete.cases(population_ville)
- 
- # La somme d'un vecteur de Boolean considère TRUE comme 1 et FALSE 0
- #____________________________________________________________________________________________  
- 
- sum_na = sum(vec_na) # compte le nbr de ligne où il n'y a pas de données manquantes
- 
- # test pour savoir si il y a des données manquantes dans les données
- #____________________________________________________________________________________________  
- 
- dim_pop[1] == sum_na
-    
+
+vec_na = complete.cases(population_ville)
+
+# La somme d'un vecteur de Boolean considère TRUE comme 1 et FALSE 0
+#____________________________________________________________________________________________  
+
+sum_na = sum(vec_na) # compte le nbr de ligne où il n'y a pas de données manquantes
+
+# test pour savoir si il y a des données manquantes dans les données
+#____________________________________________________________________________________________  
+
+dim_pop[1] == sum_na
+
 # Importer les noms des colonnes  
 #____________________________________________________________________________________________
 
-  # legende <- read.csv(paste(PATH,"legende.csv",sep=""), sep=";")
+# legende <- read.csv(paste(PATH,"legende.csv",sep=""), sep=";")
 
- legende <- read_delim("https://raw.githubusercontent.com/LaurentPolitis/Data_Viz_Fin403/master/data/legende.csv", 
-                       ";", escape_double = FALSE, locale = locale(encoding = "ISO-8859-1"), 
-                       trim_ws = TRUE)
+legende <- read_delim("https://raw.githubusercontent.com/LaurentPolitis/Data_Viz_Fin403/master/data/legende.csv", 
+                      ";", escape_double = FALSE, locale = locale(encoding = "ISO-8859-1"), 
+                      trim_ws = TRUE)
 
-  colnames(population_ville) = setNames(nm = legende$CODGEO,object = legende$`Code géographique`)[ colnames(population_ville) ]
-  
-  colnames(population_ville)[3] = "Communes"
-  
-  
-  
- 
-  
+colnames(population_ville) = setNames(nm = legende$CODGEO,object = legende$`Code géographique`)[ colnames(population_ville) ]
+
+colnames(population_ville)[3] = "Communes"
+
+colnames(population_ville)[1] = "CODGEO"
+
+
+reg2016 <- read_delim("https://raw.githubusercontent.com/lexman/code_officiel_geographique/2016/correspondance_regions.tsv", 
+                      "\t", escape_double = FALSE, trim_ws = TRUE)
+
+population_ville$Region = setNames(nm=as.numeric(reg2016$former_code) ,object = reg2016$former_name )[as.character(population_ville$Région)]
